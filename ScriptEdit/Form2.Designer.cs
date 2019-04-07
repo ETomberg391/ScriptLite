0a213cb91de890c34563bc725986f9bdc95e71be
etomberg26 <etomberg26@gmail.com>
2019-04-07 17:38:11 -0400

    Added an Automated Google Translation API button to auto-translate all lines. Will require user to input their own google cloud API Key. Will include how-to later on.

---
+++ ScriptEdit/Form2.Designer.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ScriptEdit
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.nextButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.lineHolder = new System.Windows.Forms.Label();
+            this.lineButton = new System.Windows.Forms.Button();
+            this.lineBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.lineName = new System.Windows.Forms.Label();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.lineDetail = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolOpen = new System.Windows.Forms.ToolStripButton();
+            this.toolSave = new System.Windows.Forms.ToolStripButton();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.scriptHolder = new System.Windows.Forms.RichTextBox();
+            this.translatedTextbox = new System.Windows.Forms.RichTextBox();
+            this.translatedTextHolder = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.translateButton = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(689, 492);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(72, 30);
+            this.nextButton.TabIndex = 0;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(258, 492);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(72, 30);
+            this.backButton.TabIndex = 1;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // lineHolder
+            // 
+            this.lineHolder.AutoSize = true;
+            this.lineHolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lineHolder.Location = new System.Drawing.Point(467, 25);
+            this.lineHolder.Name = "lineHolder";
+            this.lineHolder.Size = new System.Drawing.Size(67, 24);
+            this.lineHolder.TabIndex = 0;
+            this.lineHolder.Text = "Line #";
+            this.lineHolder.Click += new System.EventHandler(this.LineHolder_Click);
+            // 
+            // lineButton
+            // 
+            this.lineButton.Location = new System.Drawing.Point(961, 29);
+            this.lineButton.Name = "lineButton";
+            this.lineButton.Size = new System.Drawing.Size(39, 30);
+            this.lineButton.TabIndex = 4;
+            this.lineButton.Text = "Go!";
+            this.lineButton.UseVisualStyleBackColor = true;
+            // 
+            // lineBox
+            // 
+            this.lineBox.Location = new System.Drawing.Point(892, 35);
+            this.lineBox.MaxLength = 6;
+            this.lineBox.Name = "lineBox";
+            this.lineBox.Size = new System.Drawing.Size(63, 20);
+            this.lineBox.TabIndex = 5;
+            this.lineBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lineBox_KeyPress);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.submitButton.Location = new System.Drawing.Point(471, 492);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(72, 30);
+            this.submitButton.TabIndex = 6;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // lineName
+            // 
+            this.lineName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lineName.AutoSize = true;
+            this.lineName.Location = new System.Drawing.Point(201, 33);
+            this.lineName.Name = "lineName";
+            this.lineName.Size = new System.Drawing.Size(29, 13);
+            this.lineName.TabIndex = 7;
+            this.lineName.Text = "NPC";
+            this.lineName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lineName.Click += new System.EventHandler(this.lineName_Click);
+            // 
+            // lineDetail
+            // 
+            this.lineDetail.AutoSize = true;
+            this.lineDetail.Location = new System.Drawing.Point(820, 38);
+            this.lineDetail.Name = "lineDetail";
+            this.lineDetail.Size = new System.Drawing.Size(66, 13);
+            this.lineDetail.TabIndex = 9;
+            this.lineDetail.Text = "Skip to Line:";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolOpen,
+            this.toolSave});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1012, 25);
+            this.toolStrip1.TabIndex = 10;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolOpen
+            // 
+            this.toolOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolOpen.Image = ((System.Drawing.Image)(resources.GetObject("toolOpen.Image")));
+            this.toolOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolOpen.Name = "toolOpen";
+            this.toolOpen.Size = new System.Drawing.Size(23, 22);
+            this.toolOpen.Text = "Open";
+            // 
+            // toolSave
+            // 
+            this.toolSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolSave.Image = ((System.Drawing.Image)(resources.GetObject("toolSave.Image")));
+            this.toolSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolSave.Name = "toolSave";
+            this.toolSave.Size = new System.Drawing.Size(23, 22);
+            this.toolSave.Text = "Save";
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // scriptHolder
+            // 
+            this.scriptHolder.BackColor = System.Drawing.SystemColors.Control;
+            this.scriptHolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scriptHolder.Location = new System.Drawing.Point(0, 65);
+            this.scriptHolder.Name = "scriptHolder";
+            this.scriptHolder.ReadOnly = true;
+            this.scriptHolder.Size = new System.Drawing.Size(1012, 150);
+            this.scriptHolder.TabIndex = 11;
+            this.scriptHolder.Text = "";
+            // 
+            // translatedTextbox
+            // 
+            this.translatedTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translatedTextbox.Location = new System.Drawing.Point(0, 336);
+            this.translatedTextbox.Name = "translatedTextbox";
+            this.translatedTextbox.Size = new System.Drawing.Size(1012, 150);
+            this.translatedTextbox.TabIndex = 12;
+            this.translatedTextbox.Text = "";
+            this.translatedTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.translatedTextbox_KeyPress);
+            // 
+            // translatedTextHolder
+            // 
+            this.translatedTextHolder.BackColor = System.Drawing.SystemColors.Control;
+            this.translatedTextHolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.translatedTextHolder.Location = new System.Drawing.Point(0, 236);
+            this.translatedTextHolder.Name = "translatedTextHolder";
+            this.translatedTextHolder.ReadOnly = true;
+            this.translatedTextHolder.Size = new System.Drawing.Size(1012, 81);
+            this.translatedTextHolder.TabIndex = 13;
+            this.translatedTextHolder.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Original:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Translation:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Input:";
+            // 
+            // translateButton
+            // 
+            this.translateButton.Location = new System.Drawing.Point(12, 493);
+            this.translateButton.Name = "translateButton";
+            this.translateButton.Size = new System.Drawing.Size(72, 30);
+            this.translateButton.TabIndex = 17;
+            this.translateButton.Text = "Translate";
+            this.translateButton.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 534);
+            this.Controls.Add(this.translateButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.translatedTextHolder);
+            this.Controls.Add(this.translatedTextbox);
+            this.Controls.Add(this.scriptHolder);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.lineName);
+            this.Controls.Add(this.lineDetail);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.lineBox);
+            this.Controls.Add(this.lineButton);
+            this.Controls.Add(this.lineHolder);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.nextButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form2";
+            this.Text = "ScriptLite";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2_KeyDown);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Label lineHolder;
+        private System.Windows.Forms.Button lineButton;
+        private System.Windows.Forms.TextBox lineBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label lineName;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Label lineDetail;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolOpen;
+        private System.Windows.Forms.ToolStripButton toolSave;
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.RichTextBox scriptHolder;
+        private System.Windows.Forms.RichTextBox translatedTextbox;
+        private System.Windows.Forms.RichTextBox translatedTextHolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button translateButton;
+    }
+}
---
+++ ScriptEdit/Form2.cs
@@ -0,0 +1,270 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Translation.V2;
+
+namespace ScriptEdit
+{
+    public partial class Form2 : Form
+    {
+
+        public string[,] _result;
+        public String translatedText = "";
+        public String untranslatedText = "";
+        public int lineNum;
+        public int lineHoldernum;
+        public int holdering;
+        public int lineCounter = 0;
+        
+        public Form2()
+        {
+            InitializeComponent();
+            this.toolOpen.Click += new System.EventHandler(this.ToolOpen_Click);
+            this.toolSave.Click += new System.EventHandler(this.ToolSave_Click);
+            this.submitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            this.backButton.Click += new System.EventHandler(this.BackButton_Click);
+            this.nextButton.Click += new System.EventHandler(this.NextButton_Click);
+            this.lineButton.Click += new System.EventHandler(this.LineButton_Click);
+            this.translatedTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(CheckEnter);
+            this.translateButton.Click += new System.EventHandler(this.translateButton_Click);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            openFile.Filter = "Text files (.txt)|*.txt";
+        }
+        private void ToolOpen_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFile.FileName);
+
+                _result = new string[File.ReadLines(openFile.FileName).Count(), 4];
+                this.lineCounter = File.ReadLines(openFile.FileName).Count();
+
+                using (var streamReader = new StreamReader(openFile.FileName))
+                {
+                    var line = string.Empty;
+                    var lineCount = 0;
+                    while (line != null)
+                    {
+                        line = streamReader.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(line)) continue;
+
+                        // wait for user to enter a translation
+                        //var translation = await _translationSubmittedSource.Task;
+                        var translation = "";
+                        // split line at tabstop
+                        var parts = line.Split(new string[] { "\t" }, 2, StringSplitOptions.None); ;
+                        // populate result
+                        _result[lineCount, 0] = lineCount.ToString();
+                        _result[lineCount, 1] = parts[0];
+                        _result[lineCount, 2] = parts[1];
+                        _result[lineCount, 3] = translation;
+
+                        // reset source
+                        //_translationSubmittedSource = new TaskCompletionSource<string>();
+                        // increase line count
+                        lineCount++;
+                    }
+                }
+                holdering = Int32.Parse(_result[lineHoldernum, 0]) + 1;
+                this.untranslatedText = _result[lineHoldernum, 2];
+                this.translatedText = _result[lineHoldernum, 3];
+
+                lineHolder.Text = "Line #" + holdering.ToString();
+                lineName.Text = _result[lineHoldernum, 1];
+                scriptHolder.Text = _result[lineHoldernum, 2];
+                translatedTextHolder.Text = _result[lineHoldernum, 3];
+            }
+        }
+        private void ToolSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFile.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    for (int rowIndex = 0; rowIndex < (lineCounter); rowIndex++)
+                    {
+                        if (_result[rowIndex, 3] == "")
+                        {
+                            var line = _result[rowIndex, 1] + "\t" + _result[rowIndex, 2];
+                            sw.WriteLine(line);
+                        }
+                        else
+                        {
+                            var line = _result[rowIndex, 1] + "\t" + _result[rowIndex, 3];
+                            sw.WriteLine(line);
+                        }
+                    }
+                    MessageBox.Show("File : \n" + saveFile.FileName + "\n has successfully saved");
+                }
+            }
+        }
+        private void LineButton_Click(object sender, EventArgs e)
+        {
+            this.lineNum = Int32.Parse(lineBox.Text) - 1;
+            if (this.lineNum > this.lineCounter)
+            {
+                MessageBox.Show("Out of bounds! End of script is Line #" + lineCounter);
+            }
+            else
+            {
+                this.lineHoldernum = this.lineNum;
+
+                holdering = Int32.Parse(_result[lineHoldernum, 0]) + 1;
+                lineHolder.Text = "Line #" + holdering.ToString();
+                lineName.Text = _result[lineHoldernum, 1];
+                scriptHolder.Text = _result[lineHoldernum, 2];
+                translatedTextHolder.Text = _result[lineHoldernum, 3];
+            }
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            _result[lineHoldernum, 3] = translatedTextbox.Text.Trim();
+
+            holdering = Int32.Parse(_result[lineHoldernum, 0] + 1);
+            lineHolder.Text = "Line #" + holdering.ToString();
+            lineName.Text = _result[lineHoldernum, 1];
+            scriptHolder.Text = _result[lineHoldernum, 2];
+            translatedTextHolder.Text = _result[lineHoldernum, 3];
+        }
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            if (this.lineHoldernum != this.lineCounter - 1) {
+                this.lineHoldernum++;
+                holdering = Int32.Parse(_result[lineHoldernum, 0]) + 1;
+                lineHolder.Text = "Line #" + holdering.ToString();
+                lineName.Text = _result[lineHoldernum, 1];
+                scriptHolder.Text = _result[lineHoldernum, 2];
+                translatedTextHolder.Text = _result[lineHoldernum, 3];
+            }
+            else { };
+        }
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+
+            if (lineHoldernum != 0) {
+                this.lineHoldernum--;
+                holdering = Int32.Parse(_result[lineHoldernum, 0]) + 1;
+                lineHolder.Text = "Line #" + holdering.ToString();
+                lineName.Text = _result[lineHoldernum, 1];
+                scriptHolder.Text = _result[lineHoldernum, 2];
+                translatedTextHolder.Text = _result[lineHoldernum, 3];
+            }
+            else { };
+        }
+        private void SplitContainer1_Panel1_Paint(object sender, PaintEventArgs e) { }
+        private void SplitContainer1_Panel2_Paint(object sender, PaintEventArgs e) { }
+        private void LineHolder_Click(object sender, EventArgs e) { }
+        private void lineName_Click(object sender, EventArgs e) { }
+
+        private void lineBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void CheckEnter(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == (char)13) {
+                if (e.KeyChar == (char)13 && Control.ModifierKeys == Keys.Shift)
+                {
+                }
+                else if (e.KeyChar == (char)13) {
+
+                    _result[lineHoldernum, 3] = translatedTextbox.Text.Trim();
+
+                    holdering = Int32.Parse(_result[lineHoldernum, 0]) + 1;
+                    lineHolder.Text = "Line #" + holdering.ToString();
+                    lineName.Text = _result[lineHoldernum, 1];
+                    scriptHolder.Text = _result[lineHoldernum, 2];
+                    translatedTextHolder.Text = _result[lineHoldernum, 3];
+
+                    translatedTextbox.Text = "";
+                }
+            }
+
+        }
+        private void translatedTextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.PageUp)
+            {
+                if (lineHoldernum != 0)
+                {
+                    this.lineHoldernum--;
+                    holdering = Int32.Parse(_result[lineHoldernum, 0]) + 1;
+                    lineHolder.Text = "Line #" + holdering.ToString();
+                    lineName.Text = _result[lineHoldernum, 1];
+                    scriptHolder.Text = _result[lineHoldernum, 2];
+                    translatedTextHolder.Text = _result[lineHoldernum, 3];
+                }
+                else { };
+            }
+            if (e.KeyCode == Keys.PageDown)
+            {
+                if (this.lineHoldernum != this.lineCounter - 1)
+                {
+                    this.lineHoldernum++;
+                    holdering = Int32.Parse(_result[lineHoldernum, 0]) + 1;
+                    lineHolder.Text = "Line #" + holdering.ToString();
+                    lineName.Text = _result[lineHoldernum, 1];
+                    scriptHolder.Text = _result[lineHoldernum, 2];
+                    translatedTextHolder.Text = _result[lineHoldernum, 3];
+                }
+                else { };
+            }
+        }
+        private void translateButton_Click(object sender, EventArgs e)
+        {
+            int countFileHolder = 0;
+            int doneCount = 0;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            TranslationClient client = TranslationClient.Create();
+            for (int rowIndex = 0; rowIndex < (lineCounter); rowIndex++)
+            {
+                var response = client.TranslateText(_result[rowIndex, 2], "en");
+                _result[rowIndex, 3] = response.TranslatedText;
+                lineHolder.Text = "Line #" + _result[countFileHolder, 0].ToString();
+                lineName.Text = _result[countFileHolder, 1];
+                scriptHolder.Text = _result[countFileHolder, 2];
+                translatedTextHolder.Text = _result[countFileHolder, 3];
+                System.Threading.Thread.Sleep(500);
+                    countFileHolder++;
+                    doneCount++;
+            }
+            if(countFileHolder > 0)
+            {
+                MessageBox.Show("Translations finished!!");
+                
+            }
+            else
+            {
+                MessageBox.Show("No text was Translated");
+            }
+        }
+    }
+}
